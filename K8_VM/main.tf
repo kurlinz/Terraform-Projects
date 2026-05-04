ba275e80a9e1c9682cd0e31bee36f367ec0bff5d
kurlinz <p.cozzy2012@gmail.com>
2026-05-04 13:47:50 +0200

    test

--- K8_VM/main.tf
+++ K8_VM/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_network_security_group" "nsg" {
 
   security_rule {
     name                       = "http-allow"
-    priority                   = 102
+    priority                   = 103
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
